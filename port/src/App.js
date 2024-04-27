c8dc9fe60b0f9c67ff3e4c95a7eee45c7e2943fb
hoanhua14 <huasohoan@gmail.com>
2024-04-26 17:57:50 -0700

    finished header

---
+++ port/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './header.js';
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div ><Header/></div>
+        <div style={{backgroundColor: '#f2f2f2'}}>body
+          <h1>profile</h1>
+          <h1>Exp</h1>
+          <h1>Ed</h1>
+          <h1>skills</h1>
+          <h1>contact me</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ port/src/header.js
@@ -0,0 +1,8 @@
+import sh from './images/sh.png'
+
+const Header = () => {
+    return (
+        <div className="container"><img className='image' src={sh} alt='logo'/></div>
+    )
+}
+export default Header
