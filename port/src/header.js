7e7b0c6ae5fdf172021b9d478d0d5f46bb32caa3
hoanhua14 <huasohoan@gmail.com>
2024-06-09 19:54:01 -0700

    header

--- port/src/header.js
+++ port/src/header.js
@@ -1,8 +1,17 @@
-import sh from './images/sh.png'
+import sh from "./images/sh.png";
 
 const Header = () => {
-    return (
-        <div className="container"><img className='image' src={sh} alt='logo'/></div>
-    )
-}
-export default Header
+  return (
+    <div className="App-header">
+      <div className="container">
+        <img className="image" src={sh} alt="logo" />
+      </div>
+      <div>Profile</div>
+      <div>Exp</div>
+      <div>Ed</div>
+      <div>Skills</div>
+      <div>Contact me</div>
+    </div>
+  );
+};
+export default Header;
