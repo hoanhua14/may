f6a79214764288c74e2e18e47006ef4703b0e172
hoanhua14 <huasohoan@gmail.com>
2024-06-18 22:27:30 -0700

    fixed styling on degrees

--- port/src/Edu.js
+++ port/src/Edu.js
@@ -2,13 +2,19 @@ import hr from "./images/HR.png";
 import chabot from "./images/Chabot.png";
 const Edu = () => {
   return (
-    <div className="hr">
-      <img style={{ maxHeight: "40%", maxWidth: "40%" }} src={hr} alt="HR" />
+    <div className="degrees">
       <img
-        style={{ maxHeight: "40%", maxWidth: "40%" }}
-        src={chabot}
-        alt="chabot"
+        className="degree"
+        // style={{
+        //   maxHeight: "40%",
+        //   maxWidth: "40%",
+        //   outline: "5px black solid",
+        //   borderRadius: "5px",
+        // }}
+        src={hr}
+        alt="HR"
       />
+      <img className="degree" src={chabot} alt="chabot" />
     </div>
   );
 };
