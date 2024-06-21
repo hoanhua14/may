e246bcb1f92fc005a2b3dc9d641e99ca613888f9
hoanhua14 <huasohoan@gmail.com>
2024-06-21 13:20:23 -0700

    boxed components, fix styling

--- port/src/Edu.js
+++ port/src/Edu.js
@@ -3,17 +3,7 @@ import chabot from "./images/Chabot.png";
 const Edu = () => {
   return (
     <div className="degrees">
-      <img
-        className="degree"
-        // style={{
-        //   maxHeight: "40%",
-        //   maxWidth: "40%",
-        //   outline: "5px black solid",
-        //   borderRadius: "5px",
-        // }}
-        src={hr}
-        alt="HR"
-      />
+      <img className="degree" src={hr} alt="HR" />
       <img className="degree" src={chabot} alt="chabot" />
     </div>
   );
--- port/src/Exp.js
+++ port/src/Exp.js
@@ -4,14 +4,17 @@ const projects = [
     gitUrl: "https://github.com/hoanhua14/mrq",
     gitLogo: "https://img.icons8.com/material-rounded/24/github.png",
     description:
-      "Designed, architected, and deployed a React application with Tailwind reusable, responsive components to allow users to track, record, and view the progress of their health related goals via personalized charts and tables. Implemented authentication using JWTs for users to access protected endpoints and CI/CD  to automate the building and testing process while maintaining a working pipeline. Protected RESTful API endpoints using FastAPI framework to facilitate better and more secure users’ experience. ",
+      "Designed, architected, and deployed a React application with Tailwind reusable, responsive components to allow users to track, record, and view the progress of their health related goals via personalized charts and tables.\n Implemented authentication using JWTs for users to access protected endpoints and CI/CD  to automate the building and testing process while maintaining a working pipeline.\n Protected RESTful API endpoints using FastAPI framework to facilitate better and more secure users’ experience. ",
+    techStack:
+      "JavaScript, React.js, Python, fastAPI, Tailwind CSS, Postgresql",
   },
   {
     name: "Car.Inc",
     gitUrl: "https://github.com/hoanhua14/carinc",
     gitLogo: "https://img.icons8.com/material-rounded/24/github.png",
     description:
-      "Engineered Single Page Application (SPA) for dealership management leveraging React, seamlessly integrating microservices for servicing, sales, and inventory.Developed a Docker-based RESTful API system capable of handling data for a rapidly growing dealership. Built React component library with fetching and state management solutions for direct data via API calls. Architected object-oriented models for each microservice, ensuring efficient data organization.",
+      "Engineered Single Page Application (SPA) for dealership management leveraging React, seamlessly integrating microservices for servicing, sales, and inventory.\nDeveloped a Docker-based RESTful API system capable of handling data for a rapidly growing dealership. Built React component library with fetching and state management solutions for direct data via API calls. Architected object-oriented models for each microservice, ensuring efficient data organization.",
+    techStack: "Python, Docker, React.js, RESTful API",
   },
   {
     name: "Productive",
@@ -19,21 +22,26 @@ const projects = [
     gitLogo: "https://img.icons8.com/material-rounded/24/github.png",
     description:
       "Created and developed a project management web application using Django, Python, CSS, HTML, and SQLite to help organize, prioritize, and accomplish goals effectively, while following Agile methodologies. Empowered users to effortlessly edit tasks and projects, boosting productivity and efficiency. Protected endpoints for logged in users to create, manage, and delete records.",
+    techStack: "Python, Django",
   },
 ];
 
 const Exp = () => {
   return (
-    <div>
+    <div className="projects">
       {projects.map((project, index) => (
-        <>
-          {/* <img src={project.gitLogo} alt={project.name} /> */}
-          <a href={project.gitUrl}>
-            <img src={project.gitLogo} alt="mrq" />
-          </a>
-          <div>{project.name}</div>
-          <div>{project.description}</div>
-        </>
+        <div className="project">
+          <div className="intro">
+            {project.name}
+            <a href={project.gitUrl}>
+              <br></br>
+              <img src={project.gitLogo} alt={project.name}></img>
+              <br></br>
+            </a>
+            <div>{project.techStack}</div>
+          </div>
+          <div className="description">{project.description}</div>
+        </div>
       ))}
     </div>
   );
